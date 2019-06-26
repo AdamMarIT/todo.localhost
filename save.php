dc9b05981eae80f378446e53fdc0530750d72479
AdamMar <it.adammar@gmail.com>
2019-06-26 16:02:45 +0300

    added classes

--- save.php
+++ save.php
@@ -2,6 +2,5 @@
 
 	if ($_POST["tasksName"]){
 		$file = 'tasks.txt';
-        
         file_put_contents($file, $_POST["tasksName"]);
 	}
--- tasksActions.js
+++ tasksActions.js
@@ -1,23 +1,66 @@
-$(document).ready(function () {
-  let storage = {};
+class Task {
+
+  constructor(taskBody,  isloaded) {
+    this.taskBody = taskBody;
+    this.isloaded = isloaded;
+    this.key = Math.random().toString(36).substring(2, 15);
+  }
+
+  getHtml() {
+    let stringHtml = `
+      <div class="row item inline" data-item="${this.key}">
+        <div class="col-sm-8">
+          ${this.taskBody}&nbsp;${this.isloaded}
+        </div>
+        <div class="col-sm-2">
+          <div class="btn btn-sm btn-danger delete-button button-inline" data-item="${this.key}">
+            Delete
+          </div>
+        </div>
+      </div>`
+    return stringHtml;
+  }
+}
+
+class Storage {
+
+  constructor() {
+    this.storage = {};
+  }
+
+  add(task) {
+    this.storage[task.key] = task.taskBody;
+  }
+
+  remove(taskKey) {
+    delete this.storage[taskKey];
+  }
+
+  getJson(){
+    return JSON.stringify(this.storage);
+  }
+}
+
+$(document).ready(function () { 
+  const storage = new Storage();
 		  
   $('#add-button').click(function () { 
-    let value = $('#new-item').val(); 
-    let addTasks = addTask(value, '');
+    let taskBody = $('#new-item').val(); 
+    addTask(taskBody, '');
 
     $('#new-item').val('');				     
   });
 
   $('#list').on("click", '.delete-button', function () { 
     let randomThis = $(this).attr('data-item');
-    delete storage[randomThis];
+    storage.remove(randomThis);
 
     $(`.item[data-item="${randomThis}"]`).remove(); 
     return false;
   });
 
   $('#save-button').click(function () {
-    let tasksStr = JSON.stringify(storage);
+    let tasksStr = storage.getJson();
 
     $.ajax('/save.php', {
   	  type: 'POST',  
@@ -32,36 +75,23 @@ $(document).ready(function () {
   	});
   });
 
-$('#load-button').click(function () {
-  $.ajax('/load.php', {    
-	success: function (data, status, xhr) {
-	  let storageLoaded = JSON.parse(data);
-
-	  for (let key in storageLoaded) { 
-	    let addTasks = addTask(storageLoaded[key], '(loaded)');
-	  }
-	},
-	error: function (jqXhr, textStatus, errorMessage) {
-	}
+  $('#load-button').click(function () {
+    $.ajax('/load.php', {    
+    	success: function (data, status, xhr) {
+    	  let storageLoaded = JSON.parse(data); 
+    	  for (let key in storageLoaded) { 
+    	    addTask(storageLoaded[key], '(loaded)');
+    	  }
+    	},
+    	error: function (jqXhr, textStatus, errorMessage) {
+    	}
+    });
   });
-});
 
-  function addTask(value, isloaded) {
-    let random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    random = random.trim();
-  				  
-    $('#list').append(`
-      <div class="row item inline" data-item="${random}">
-        <div class="col-sm-8">
-          ${value}&nbsp;${isloaded}
-        </div>
-  	  <div class="col-sm-2">
-  	    <div class="btn btn-sm btn-danger delete-button button-inline" data-item="${random}">
-  	      Delete
-  	    </div>
-        </div>
-      </div>`
-     )
-     storage[random] = value;	
+  function addTask(taskBody, isloaded) {
+    const task = new Task(taskBody, isloaded); 				  
+    $('#list').append(task.getHtml());
+    storage.add(task)
   }
+
 });
